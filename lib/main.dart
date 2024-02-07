7d9e382d4480012f93c65db20c7df84d7e43402a
shad <shadislam999@gmail.com>
2024-02-08 00:08:55 +0600

    funtion list build

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -35,30 +36,15 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List <Icon>scoreKeeper = [
+  List <Icon>scoreKeeper = [];
+  List<String>questions =[
+      'You can lead a cow down stairs but not up stairs.',
+      'Approximately one quarter of human are in the feet.'
+      'A slug\'s  blood is green.',
 
-       Icon(
-                Icons.check,
-                color: Colors.green,
-              ),
-              Icon(
-                Icons.close,
-                color: Colors.red,
-              ),
-               Icon(
-                Icons.close,
-                color: Colors.red,
-              ),
-               Icon(
-                Icons.close,
-                color: Colors.red,
-              ),
-               Icon(
-                Icons.close,
-                color: Colors.red,
-              ),
   ];
-
+  
+  int questionNumber= 0;
  
   @override
   Widget build(BuildContext context) {
@@ -73,7 +59,7 @@ class _QuizPageState extends State<QuizPage> {
             padding:EdgeInsets.all(10.0) ,
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -94,13 +80,13 @@ class _QuizPageState extends State<QuizPage> {
             backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
               ),
                onPressed: () {
+                setState(() {
+                  questionNumber = questionNumber+1;
+                });
         // The user picked true.
-        setState(() {
           
-          scoreKeeper.add(
-                  Icon(Icons.check,color: Colors.green,),
-                 );
-        });
+           print(questionNumber);
+        
                  
              },
              child: Text(
@@ -123,7 +109,11 @@ class _QuizPageState extends State<QuizPage> {
           
         ),
         onPressed: () {
+          setState(() {
+            questionNumber = questionNumber+1;
+          });
           
+          print(questionNumber);
         },
         child: Text(
           'False',
